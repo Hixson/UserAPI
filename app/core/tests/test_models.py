304e9e961f664243d9ec73d3b14bb5141d7fa8e1
Michael Hixson <hixsonweb@gmail.com>
2019-09-29 13:19:33 -0500

    Updated admin to support custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        """test creating a new user with an email is successful"""
+        """Test creating a new user with an email is successful"""
         email = 'test@michaelhixson.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
